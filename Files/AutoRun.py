d3e3ad763ee35aaa451d5c20e1a4985591d46640
HappyQQ <happyqq@me.com>
2019-11-10 21:04:14 +0800

    test
    test

---
+++ Files/AutoRun.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python  
+#-*- coding: utf-8 -*-
+import socket
+import urllib.request
+import urllib.error
+import os
+import shutil
+import configparser
+import platform
+from git import Repo
+
+if __name__ == "__main__":
+	print("Autorun Now!!!")
+
+
+
